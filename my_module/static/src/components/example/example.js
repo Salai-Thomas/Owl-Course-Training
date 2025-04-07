01c0ce0db33191e92824e97ac6788e184b57c5f3
SalaiThomas <salaithomas582@gmail.com>
2025-04-07 18:59:13 +0630

    registry component widget

--- my_module/static/src/components/example/example.js
+++ my_module/static/src/components/example/example.js
@@ -1,5 +1,9 @@
 import {Component} from "@odoo/owl";
+import { registry } from '@web/core/registry';
 
 export class Example extends Component{
-    static template = "my_module.Example"
+    static template = "my_module.Example";
+
 }
+
+registry.category('view_widgets').add("example",{component:Example});
