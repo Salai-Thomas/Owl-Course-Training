a112fd1c7e1a2a9aec588b980fc8da81fde1e7b8
SalaiThomas <salaithomas582@gmail.com>
2025-04-07 06:53:55 +0630

    environment setup

---
+++ my_module/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Module",
+    'version': '18.0.1.0.0',
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'depends': ['base'],
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'my_module/static/src/**/*',
+        ],
+    }
+
+}
---
+++ my_module/static/src/components/example/example.js
@@ -0,0 +1,5 @@
+import {Component} from "@odoo/owl";
+
+export class Example extends Component{
+    static template = "my_module.Example"
+}
