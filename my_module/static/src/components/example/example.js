31fe3506e7bab152cd277cf1bafdd10bb56447aa
SalaiThomas <salaithomas582@gmail.com>
2025-04-10 08:51:26 +0630

    use child subcomponent and string props pass

--- my_module/static/src/components/example/example.js
+++ my_module/static/src/components/example/example.js
@@ -1,8 +1,11 @@
 import {Component} from "@odoo/owl";
+import {Child} from "../child/child";
 import { registry } from '@web/core/registry';
 
 export class Example extends Component{
     static template = "my_module.Example";
+
+    static components = {Child};
 
 }
 
